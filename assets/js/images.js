3a6e28feef30e71930a7391b85fd8a6164b8b534
douglasrubims <douglasrubims@gmail.com>
2021-03-18 01:13:04 -0300

    Melhorias no sistema

--- assets/js/images.js
+++ assets/js/images.js
@@ -2,8 +2,9 @@ async function getImage(id) {
 	const req = new Request('https://api.github.com/users/' + id);
 	req.headers.append('Accept', 'application/vnd.github.v3+json');
 	const fetchedReq = await fetch(req);
-	const { avatar_url } = await fetchedReq.json();
+	const { avatar_url, html_url } = await fetchedReq.json();
 	const avatarElement = document.getElementById(id);
+	avatarElement.href = html_url;
 	avatarElement.style.background = `url('${ avatar_url }')`;
 	avatarElement.style.backgroundSize = "100% 100%";
 }
