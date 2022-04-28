1f65cbea1fcce295c93adea66fcdf4af3accaa6a
Douglas Rubim <douglasrubims@gmail.com>
2022-04-27 23:01:07 -0300

    Apaguei tudo e é isso aí

--- assets/js/landing.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('btn-menu').addEventListener('click', e => {
-	document.getElementById('navbar-main').classList.toggle('over');
-	document.getElementById('navbar-menu').classList.toggle('show');
-});
-
-const showItem = ({ id }) => document.getElementById(id + '-content').classList.toggle('show');
